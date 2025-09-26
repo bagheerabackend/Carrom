e345353e94f34f6ee13528ee9d884da547ece07f
Bagheera Backend <bagheerabackend@gmail.com>
2025-09-26 13:10:27 +0530

    fix: Update cron installation command to use sudo for compatibility

--- BagheeraCarrom/urls.py
+++ BagheeraCarrom/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('api/', api.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
